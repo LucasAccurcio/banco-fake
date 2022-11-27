4bbf19ddd3e08f1da168ca7aa8953c53377bbe45
LucasAccurcio <lucas.accurcio@gmail.com>
2022-11-27 15:55:35 -0300

    feat: :lock: add auth service

---
+++ src/app/shared/services/auth.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { User } from '../interfaces/user.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  user: User;
+  token: string;
+
+  constructor(private router: Router) { }
+
+  setUser(user: User) {
+    this.user = user;
+    localStorage.setItem('user', JSON.stringify(user));
+  }
+
+  getUser() {
+    if (this.user) {
+      return this.user;
+    }
+
+    const userStored = localStorage.getItem('user');
+    if (userStored) {
+      this.user = JSON.parse(userStored);
+      return this.user;
+    }
+
+    return null;
+  }
+
+  setToken(token: string) {
+    this.token = token;
+    localStorage.setItem('token', token);
+  }
+
+  getToken() {
+    if (this.token) {
+      return this.token;
+    }
+
+    const tokenStored = localStorage.getItem('token');
+    if (tokenStored) {
+      this.token = tokenStored;
+      return this.token;
+    }
+
+    return null;
+  }
+
+  isLogged(): boolean {
+    if (this.getUser() && this.getToken()) {
+      return true;
+    }
+
+    return false;
+  }
+
+  logout() {
+    this.user = null;
+    this.token = null;
+    localStorage.clear();
+    this.router.navigate(['login']);
+  }
+}

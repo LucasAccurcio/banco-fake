60a9c87ab903e31d741901e982000147e8dfda36
LucasAccurcio <lucas.accurcio@gmail.com>
2022-11-29 22:04:10 -0300

    feat: :sparkles: add some cards to component

--- src/app/logged/home/home.component.ts
+++ src/app/logged/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/shared/interfaces/user.interface';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  user: User | undefined;
+
+
+  constructor(private authService: AuthService) {
+    this.user = this.authService.getUser();
+    console.log(this.user);
+  }
 }
